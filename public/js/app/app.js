f9b98e01058ef6f630f8845f124963d776a65a9e
Pramod Bharadwaj <Pramod Bharadwaj>
2016-06-11 16:45:43 +0530

    notification_links_using_onclick

--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,6 +1,6 @@
 var basepath = "/templates";
 
-var Caweb = angular.module('Caweb', ['ngMaterial', 'ngRoute', 'restangular', 'ngMessages', 
+var Caweb = angular.module('Caweb', ['ngMaterial', 'ngRoute', 'restangular', 'ngMessages',
 	'md.data.table', 'ngFileUpload', 'mdPickers']);
 
 Caweb.constant('Tabs', [
@@ -95,13 +95,16 @@ Caweb.config(function($mdThemingProvider, RestangularProvider, $routeProvider, $
         		}]
         	}
         })
-        .otherwise('/dashboard');
+                  .otherwise('/dashboard');
 });
 
 Caweb.run(function($rootScope, UserService, $mdToast, Tabs, $location, CAService){
 	$rootScope.user = UserService.getUserDetails();
 	$rootScope.tabs = Tabs;
 	$rootScope.tabsMap = {};
+	$rootScope.viewTask = function(item) {
+		$location.path('/task/'+[[item.task_id]]);
+	}
 	for(i in $rootScope.tabs) {
 		$rootScope.tabsMap[$rootScope.tabs[i]] = parseInt(i);
 	}
@@ -113,6 +116,7 @@ Caweb.run(function($rootScope, UserService, $mdToast, Tabs, $location, CAService
         this.PAGE_SIZE = 10;
         this.fetchNumItems_();
     };
+
         // Required.
         Notifications.prototype.getItemAtIndex = function(index) {
           var pageNumber = Math.ceil(index / this.PAGE_SIZE)?Math.ceil(index / this.PAGE_SIZE):1;
