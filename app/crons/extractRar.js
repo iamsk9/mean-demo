cc9c2a1079cc043a7255960da43022b3ff7b8533
haresh <haresh93@gmail.com>
2016-06-21 22:28:03 +0530

    changes for processing the job

--- app/crons/extractRar.js
+++ app/crons/extractRar.js
@@ -1,18 +1,35 @@
 var kue = require('kue');
 
+var config = require('../../config/config.js');
+
 var admZip = require('adm-zip');
 
 var azureStorage = require('azure-storage');
+
+var DocsHelper = require('../helpers/docsHelper');
+
+var blobService = azureStorage.createBlobService(config.storage.storageAccount, config.storage.storageAccessKey);
 
 var jobs = kue.createQueue();
 
 jobs.process('extractRar', function(job, done) {
 	console.log(job.data);
 	var zip = new admZip(job.data.file.path);
-	zip.extractAllTo('uploads/' + job.data.file.originalname.substr(0, job.data.file.originalname.length - 4), true);
+	var extractedFolderName = job.data.file.originalname.substr(0, job.data.file.originalname.length - 4);
+	zip.extractAllTo('uploads/' + extractedFolderName, true);
 	zip.getEntries().forEach(function(zipEntry) {
 		console.log(zipEntry);
-		azureStorage.createBlockBlobFromLocalFile('');
+		console.log(zipEntry.entryName);
+		console.log(zipEntry.isDirectory);
+		if(zipEntry.isDirectory) {
+			var filePath = zipEntry.entryName.split('/');
+			var params = {
+				client_id : job.data.client_id,
+
+			}
+			db.
+		}
+		//blobService.createBlockBlobFromLocalFile('');
 	});
 	done();
 });
